c5ffe4982353b770cd607dbbb5491f33fae2057c
Jose Pedro Vieira do Nascimento Filho <j.pedronascimentofilho@gmail.com>
2020-07-04 18:45:56 -0300

    Add code that solves the challenge

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransectionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((acumulador, transaction) => {
+      if (transaction.type === 'income') {
+        return acumulador + transaction.value;
+      }
+      return acumulador + 0;
+    }, 0);
+
+    const outcome = this.transactions.reduce((acumulador, transaction) => {
+      if (transaction.type === 'outcome') {
+        return acumulador + transaction.value;
+      }
+      return acumulador + 0;
+    }, 0);
+
+    const balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransectionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
